5cb2453b8f8b0bd57eaf89d172ac86b5f9cc0338
Uzair Said <bitanguzair@gmail.com>
2024-08-06 22:37:36 +0200

    using a model

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,5 +1,6 @@
 import { Component,EventEmitter,Inject, Input, OnInit, Output } from '@angular/core';
 import { CurrencyPipe, NgStyle, UpperCasePipe } from '@angular/common';
+import { Shoe } from '../models/shoe';
 
 @Component({
   selector: 'app-article-list',
@@ -7,74 +8,37 @@ import { CurrencyPipe, NgStyle, UpperCasePipe } from '@angular/common';
   imports: [NgStyle,CurrencyPipe,UpperCasePipe],
   templateUrl: './article-list.component.html',
   styleUrl: './article-list.component.scss',
-  providers: [
-    {provide: 'ARTICLE_NAME',useValue:null},
-    {provide: 'IMAGE',useValue:null},
-    {provide: 'PRICE',useValue:null},
-    {provide: 'COUNT',useValue:null},
-    {provide: 'AVAILABLE',useValue:null},
-
-  ]
+  
 })
 export class ArticleListComponent implements OnInit{
-  articleName!: string;
-  image!: string;
-  price!: number;
-  count!: number;
+  
   isAvailable!:boolean;
   isButtonActive!:boolean;
-  articleList!: ArticleListComponent [];
+  articleList!: Shoe [];
 
 
-  constructor(@Inject('ARTICLE_NAME') articleName:string,@Inject('IMAGE') image:string,@Inject('PRICE') price:number, @Inject('COUNT') count:number) {
-    this.articleName = articleName;
-    this.image = image;
-    this.price = price;
-    this.count = count;
-    this.isAvailable = true;
-    this.isButtonActive = true;
-
-  }
-  ngOnCreate(): ArticleListComponent[]{
-    let article = new ArticleListComponent("air jordi","../../assets/shoe_nike.jpeg",20000,3);
-    let article1 = new ArticleListComponent("Nike","../../assets/images.jpeg",30000,3);
-    let article2 = new ArticleListComponent("Vans","../../assets/shoe.jpeg",25000,3);
+  ngOnCreate(): Shoe[]{
+    let article = new Shoe(1,"air jordi","../../assets/shoe_nike.jpeg",20000,3);
+    let article1 = new Shoe(2,"Nike","../../assets/images.jpeg",30000,3);
+    let article2 = new Shoe(3,"Vans","../../assets/shoe.jpeg",25000,3);
 
     this.articleList = [article,article1,article2];
     return this.articleList;
   }
 
   ngOnInit(): void {
-    let articles = this.ngOnCreate()
-
-    // for (let i = 0; i < articles.length; i++) {
-    //   this.articleName = articles[i].articleName;
-    //   this.image = articles[i].image;
-    //   this.price = articles[i].price; 
-    // }
+    let articles = this.ngOnCreate();
     this.articleList = articles;
-
-    // this.articleName = article.articleName
-    // this.image = article.image
-    // this.price = article.price
-    // this.count = article.count
-
-    // console.log(article+"niyi")
-    // article.isButtonActive= true;
-    // if(article.count === 0 ){
-    //   article.isAvailable =false;
-    // }
-  
   }
-onBuy(): void{
-  console.log("niyi erga"+this.count)
-  if (this.count !==0){
-    this.count--;
-    console.log("count: "+this.count);
-  }
-  else {
-    this.isAvailable = false;
-    this.isButtonActive = false;
-  }
-}
+// onBuy(): void{
+//   console.log("niyi erga"+this.count)
+//   if (this.count !==0){
+//     this.count--;
+//     console.log("count: "+this.count);
+//   }
+//   else {
+//     this.isAvailable = false;
+//     this.isButtonActive = false;
+//   }
+// }
 }
---
+++ src/app/models/shoe.ts
@@ -0,0 +1,10 @@
+export class Shoe {
+
+    constructor(
+            public id: number,
+            public name: string,
+            public image: string,
+            public price: number,
+            public count: number,
+    ){};
+}

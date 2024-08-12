69be0d5c9c0fa8540089769874e1a92ad838c15c
Uzair Said <bitanguzair@gmail.com>
2024-08-12 17:10:52 +0200

    Added cart total using obsevable

--- src/app/Services/cart.service.ts
+++ src/app/Services/cart.service.ts
@@ -1,27 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Shoe } from '../models/shoe';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   private cart: Shoe[] = [];
-  private total: number =0;
+  private total = new BehaviorSubject<number>(this.calculateTotal());
+  total$ = this.total.asObservable()
 
   getCart():Shoe[]{
     return this.cart;
   }
-  getTotal():number{
-    return this.total;
-  }
+
 
   addCart(shoe: Shoe){
     this.cart.push(shoe);
     this.updateTotal();
   }
-  updateTotal():void{
-    this.total = this.cart.reduce((sum,shoe)=>sum+shoe.price,0);
+  calculateTotal():number{
+    return this.cart.reduce((sum,shoe)=>sum+shoe.price,0);
+  }
 
+  updateTotal():void{
+    this.total.next(this.calculateTotal())
   }
   
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,24 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CartService } from '../Services/cart.service';
 import { Shoe } from '../models/shoe';
 import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-cart',
   standalone: true,
   imports: [NgFor],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrl: './cart.component.scss',
+  
 })
-export class CartComponent{
+export class CartComponent implements OnInit{
   cart:Shoe[]  = [];
-  total:number = 0;
-  constructor(private cartService: CartService,){
-    this.cart= this.cartService.getCart();
-    this.total = this.cartService.getTotal();
-
+  total:number =0;
+  constructor(private cartService: CartService){
+  }
+  ngOnInit(): void {
+    this.cart = this.cartService.getCart();
+    this.cartService.total$.subscribe(value=>{
+      this.total = value;
+    })
   }
   
-
-
 }

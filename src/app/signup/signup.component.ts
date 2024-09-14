1e9c72bc3343ee38e9fb27c84566c38677eea5e4
Uzair Said <bitanguzair@gmail.com>
2024-09-14 11:42:00 +0200

    Validate react forms

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 
+import { NgIf } from '@angular/common';
 import { Component} from '@angular/core';
 import {FormBuilder,ReactiveFormsModule, Validators} from '@angular/forms';
 
@@ -9,21 +10,34 @@ import {FormBuilder,ReactiveFormsModule, Validators} from '@angular/forms';
   standalone: true,
   imports: [
     ReactiveFormsModule,
+    NgIf
   
   ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class SignupComponent {
-  constructor(private formBuilder: FormBuilder) {
-  }
+export class SignupComponent{
+  constructor(private formBuilder: FormBuilder) {}
 
   signupForm = this.formBuilder.group({
   name: ['', Validators.required],
-  email: ['', Validators.email],
-  password: [''],
-  confirmPassword: ['']
+  email: ['',Validators.required, Validators.email],
+  password: ['',Validators.required, Validators.minLength(8), Validators.maxLength(16)],
+  confirmPassword: ['', Validators.required,Validators.minLength(8), Validators.maxLength(16)]
 });
+
+get name() {
+  return this.signupForm.get("name");
+}
+get email() {
+  return this.signupForm.get("email");
+}
+get password() {
+  return this.signupForm.get("password");
+}
+get confirmPassword() {
+  return this.signupForm.get("confirmPassword");
+}
    
   
  onSubmit():void{
